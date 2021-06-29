816a082ccd564d6a8c6d13a3d307b4a1c2f6da90
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-29 04:10:49 -0300

    Exercícios - Sistema de filtro de comentários

--- classe-01/index.js
+++ classe-01/index.js
@@ -1,1 +1,11 @@
-const comentario = "Esse COVID é muito perigoso!";
+const comentario = "Esse PANDEMIA é muito perigoso!";
+
+if (comentario.includes("COVID") || comentario.includes("covid") || comentario.includes("Covid")) {
+    console.log("Comentário bloqueado por conter palavras proibidas");
+} else if (comentario.includes("PANDEMIA") || comentario.includes("pandemia") || comentario.includes("Pandemia")) {
+    console.log("Comentário bloqueado por conter palavras proibidas");
+} else {
+
+    console.log("Comentário autorizado");
+}
+
