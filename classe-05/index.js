ad2a64a87856323ded07a3c47791bed0de727db3
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-29 04:12:57 -0300

    Exercícios - Sistema para biblioteca

--- classe-05/index.js
+++ classe-05/index.js
@@ -1,2 +1,9 @@
 const livros = ['Guerra e Paz', 'A Montanha Mágica', 'Cem Anos de Solidão', 'Dom Quixote', 'A Divina Comédia'];
-const nomeDoLivro = "Dom Quixote";
+const nomeDoLivro = 'Guerra e Paz';
+
+
+if (livros.includes(nomeDoLivro)) {
+    console.log(livros.indexOf(nomeDoLivro) + 1);
+} else {
+    console.log("Livro não localizado");
+}
