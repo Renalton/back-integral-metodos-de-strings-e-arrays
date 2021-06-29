aa9bb71c5c3b91d2391b9356ea9e77d878ae5c86
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-29 04:13:23 -0300

    Exercícios - Teste suas habilidades

--- classe-06/index.js
+++ classe-06/index.js
@@ -1,1 +1,12 @@
 const frutas = ['Banana', 'Maçã', 'Abacaxi', 'Pêra', 'Uva'];
+
+
+console.log(frutas.reverse());
+console.log(frutas);
+
+frutas.splice(0, 1);
+console.log(frutas);
+frutas.splice(frutas.length-1, 1, "Melão");
+console.log(frutas);
+
+
