67cfb088c34159915b630cb9be3a41d74ba3e2a0
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-29 04:11:53 -0300

    Exercícios - URL amigável

--- classe-03/index.js
+++ classe-03/index.js
@@ -1,1 +1,11 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
+
+let novoTexto = "";
+let textoAntigo = texto;
+while (textoAntigo !== novoTexto) {
+    novoTexto = textoAntigo;
+    textoAntigo = textoAntigo.replace(" ", "-");
+}
+
+
+console.log(novoTexto);
